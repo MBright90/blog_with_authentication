652ba4c4eae3c7e5b52f4907d46a72d09777eaf9
Mike <michael.bright90@icloud.com>
2022-03-06 16:38:00 +0000

    Adding psycopg2-binary for postgreSQL database

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ from forms import CreatePostForm, CreateUserForm, LoginForm, CreateComment
 from flask_gravatar import Gravatar
 from functools import wraps
 
+import os
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app,
@@ -21,7 +23,7 @@ gravatar = Gravatar(app,
 
 
 # *** # CONNECT TO BLOG DB # *** #
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
